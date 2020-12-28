88644912a24c6bb18fcdc3cd4f853d3c413f3d40
Kyustern <leont562@gmail.com>
2020-12-29 00:09:01 +0100

    hh

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json())
 
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 //API
